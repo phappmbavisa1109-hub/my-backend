9ead83d4be1aaa15b56a41fded10e193fa573c73
khanhtho3110 <khanhtho3110@gmail.com>
2025-11-12 22:43:40 +0700

    fix: replace bcrypt with bcryptjs (pure JS, Cloudflare Workers compatible)

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -5,7 +5,7 @@ import { Repository } from 'typeorm';
 import { User } from '../entities/user.entity';
 import { CreateUserDto } from './dto/create-user.dto';
 import { LoginDto } from './dto/login.dto';
-import * as bcrypt from 'bcrypt';
+import * as bcrypt from 'bcryptjs';
 
 @Injectable()
 export class AuthService {
